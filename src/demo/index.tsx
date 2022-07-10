45bb601cc56aa96b3e4783fa6943a736ba966e6a
raedsalah <dkraed@gmail.com>
2022-07-10 18:59:00 +0800

    some cleanup

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as ReactDOMClient from "react-dom/client";
 
 //Component
@@ -7,10 +7,14 @@ import OtpInput from "../lib";
 const container = document.getElementById("app");
 const root = ReactDOMClient.createRoot(container!);
 
-const Demo = () => {
+const Demo = ({ }) => {
+
+  const [numOfFields, setNumOfFields] = useState(0)
   return (
-    <div>
-      <OtpInput />
+    <div className="wrapper">
+      <div className="content-wrapper">
+        <OtpInput numOfFields={numOfFields} />
+      </div>
     </div>
   );
 };
