95d8149e51b8f4957ad70d9ada4e7c93e13af345
Raed Salah <dkraed@gmail.com>
2022-07-15 01:08:09 +0800

    demo init layout

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import * as ReactDOMClient from "react-dom/client";
 
+//styles
+import "./style.css"
+
 //Component
 import OtpInput from "../lib";
 
@@ -9,10 +12,15 @@ const root = ReactDOMClient.createRoot(container!);
 
 const Demo = ({ }) => {
 
-  const [numOfFields, setNumOfFields] = useState(0)
+  const [numOfFields, setNumOfFields] = useState(2)
   return (
     <div className="wrapper">
+      <div className="sidepanel">
+        <label>Number of fields</label>
+        <input type="number" min="2" max="12" value={numOfFields} onChange={e => setNumOfFields(e.target.value)} />
+      </div>
       <div className="content-wrapper">
+
         <OtpInput numOfFields={numOfFields} />
       </div>
     </div>
